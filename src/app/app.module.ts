e703d2ae7d17afb620bb9277d58b64d19c2705b0
GabrielLourencoh <gl930551@gmail.com>
2025-06-13 12:20:07 -0300

    Instalando o typeorm pg com 'npm install @nestjs/typeorm pg', instalando o class-validator e o class-transformer com 'npm install class-validator class-transformer, criando o banco de dados local postgreSQL e estabelecendo sua conexão com o typeORM no modulo raiz (app.module.ts)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432, // Porta padrao
+      username: 'postgres', //usernamepadrao
+      database: 'biblioteca_digital', //nome do banco
+      password: '240024',
+      autoLoadEntities: true, // Carrega entidades sem precisar especificá-las
+      synchronize: true, // Sincroniza com o BD. Não deve ser usado em produção
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
