7a07c57ebb9acc32f32183d7041762a6301dfe9e
GabrielLourencoh <gl930551@gmail.com>
2025-06-13 13:38:11 -0300

    Criando arquivos para os Autores e para os Livros que são a logica da nossa aplicação e criando a entidade do Autor e entidade do Livro

---
+++ src/livros/entities/livro.entity.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsNumber, IsPositive, IsString } from 'class-validator';
+import { Autor } from 'src/autores/entities/autor.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Livro {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100, nullable: false })
+  @IsString()
+  titulo: string;
+
+  @Column({ length: 13, unique: true, nullable: false }) // ISBN tem 10 ou 13 dígitos
+  @IsString()
+  isbn: string;
+
+  @Column({ length: 40, nullable: false })
+  @IsString()
+  genero: string;
+
+  @Column({ nullable: false })
+  @IsNumber()
+  @IsPositive()
+  anoPublicacao: number;
+
+  @Column()
+  @IsNumber()
+  @IsPositive()
+  paginas: number;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+
+  // --- RELAÇÃO: Um livro pertence a um autor ---
+  @ManyToOne(
+    () => Autor, // Entidade alvo: um Livro tem um único Autor
+    (autor) => autor.livros, // Lado inverso: a propriedade 'livros' em Autor aponta de volta para Livro
+  )
+  @JoinColumn({ name: 'autorId' }) // Define a coluna 'autorId' como chave estrangeira nesta tabela
+  autor: Autor; // Propriedade para carregar o objeto Autor completo
+
+  @Column({ nullable: false }) // Coluna física para armazenar o ID do autor no banco de dados
+  autorId: number; // Chave estrangeira que referencia a tabela Autor
+}
