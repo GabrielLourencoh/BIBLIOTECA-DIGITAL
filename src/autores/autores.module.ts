89d9d04c20be6cd3c3870eeb34eb5c51063e1dce
GabrielLourencoh <gl930551@gmail.com>
2025-06-17 18:47:49 -0300

    Importando o PrismaModule para dentro do autores.module.ts, para o livros.module.ts e retirando o uso do TypeORM de ambos

--- src/autores/autores.module.ts
+++ src/autores/autores.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AutoresService } from './autores.service';
 import { AutoresController } from './autores.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Autor } from './entities/autor.entity';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Autor } from './entities/autor.entity';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Autor])], // Registra a entidade Autor para este módulo
+  imports: [
+    // TypeOrmModule.forFeature([Autor])
+    PrismaModule,
+  ], // Registra a entidade Autor para este módulo
   controllers: [AutoresController],
   providers: [AutoresService],
   exports: [AutoresService],
--- src/livros/livros.module.ts
+++ src/livros/livros.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { LivrosService } from './livros.service';
 import { LivrosController } from './livros.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Livro } from './entities/livro.entity';
-import { Autor } from 'src/autores/entities/autor.entity';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Livro } from './entities/livro.entity';
+// import { Autor } from 'src/autores/entities/autor.entity';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Livro, Autor])], // Registra a entidade Livro e Autor para este módulo
+  imports: [
+    // TypeOrmModule.forFeature([Livro, Autor])
+    PrismaModule,
+  ], // Registra a entidade Livro e Autor para este módulo
   controllers: [LivrosController],
   providers: [LivrosService],
 })
