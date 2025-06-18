93dfe0b7d5491583643a38a9b949b4f1643e1ba8
GabrielLourencoh <gl930551@gmail.com>
2025-06-18 18:12:03 -0300

    Instalando o Swagger para o nosso projeto e realizando as devidas configurações no nosso 'main.ts'

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,27 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+// Importando módulos do Swagger
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(), // <- Dizendo ao NestJS para usar o Fastify
   );
+
+  // Definindo as configurações da documentação Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Biblioteca Digital') // Titulo da documentação
+    .setDescription('API de gerenciamento de autores e livros') // Descrição opcional
+    .setVersion('1.0') // Versão da API
+    .build();
+
+  // Cria o documento Swagger com base na config e nos endpoints existentes
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Ativa a interface Swagger na rota /api (ex: http://localhos:3000/api)
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
