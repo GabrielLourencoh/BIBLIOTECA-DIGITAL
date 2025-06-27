03dae59a4cb2bbe91dae5994e5d4e1863f077a6d
GabrielLourencoh <gl930551@gmail.com>
2025-06-27 14:55:44 -0300

    Organizando o projeto em Arquitetura Limpa e estrutura DDD, parte 13: Refatorando o FindAllAutorUseCase (find-all-autor.use-case.ts) para o uso da Entidade Autor

--- src/modules/autores/application/use-cases/find-all-autor.use-case.ts
+++ src/modules/autores/application/use-cases/find-all-autor.use-case.ts
@@ -1,12 +1,12 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AutorRepository } from '../domain/repositories/autor.repository';
-import { Autor } from '@prisma/client';
+import { Autor as DomainAutorEntity } from '../domain/entities/autor.entity';
 
 @Injectable()
 export class FindAllAutorUseCase {
   constructor(private readonly autorRepository: AutorRepository) {} // Injetando a interface
 
-  async execute(): Promise<Autor[]> {
+  async execute(): Promise<DomainAutorEntity[]> {
     try {
       const autores = await this.autorRepository.findAll();
       return autores;
