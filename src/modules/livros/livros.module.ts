7182042b8355e698b6e66cc31c082a8672640e9c
GabrielLourencoh <gl930551@gmail.com>
2025-06-30 13:16:30 -0300

    Organizando o projeto em Arquitetura Limpa e estrutura DDD, parte 28: Configurando o livros.module.ts para receber as novas definições da aplicação

--- src/modules/livros/livros.module.ts
+++ src/modules/livros/livros.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
-import { LivrosService } from './livros.service';
-import { LivrosController } from './livros.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { CreateLivroController } from './application/presentation/controllers/create-livro.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateLivroUseCase } from './application/use-cases/create-livro.use-case';
+import { LivroRepository } from './application/domain/repositories/livro.repository';
+import { PrismaLivroRepository } from './application/infra/prisma/prisma-livro.repository';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [LivrosController],
-  providers: [LivrosService],
+  controllers: [CreateLivroController],
+  providers: [
+    PrismaService,
+    CreateLivroUseCase,
+    {
+      provide: LivroRepository,
+      useClass: PrismaLivroRepository,
+    },
+  ],
 })
 export class LivrosModule {}
