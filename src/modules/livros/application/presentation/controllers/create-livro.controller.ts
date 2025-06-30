ab12241e0449a23a4b2f2862417a169d01d37b6b
GabrielLourencoh <gl930551@gmail.com>
2025-06-30 13:09:26 -0300

    Organizando o projeto em Arquitetura Limpa e estrutura DDD, parte 27: Criando o create-livro.controller.ts para rota de requisição do método create

---
+++ src/modules/livros/application/presentation/controllers/create-livro.controller.ts
@@ -0,0 +1,22 @@
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateLivroUseCase } from '../../use-cases/create-livro.use-case';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { CreateLivroDto } from '../dtos/inputs/create-livro.dto';
+
+@ApiTags('livros')
+@Controller('livros')
+export class CreateLivroController {
+  constructor(private readonly createLivroUseCase: CreateLivroUseCase) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Criar um novo livro' })
+  @ApiResponse({ status: 201, description: 'Livro criado com sucesso' })
+  @ApiResponse({ status: 409, description: 'ISBN duplicado' })
+  @ApiResponse({ status: 500, description: 'Erro interno do servidor' })
+  async create(@Body() createLivroDto: CreateLivroDto) {
+    // Delega a lógica de negócio para o Use Case.
+    // O Use Case retorna a Entidade de Domínio Livro criada.
+    return this.createLivroUseCase.execute(createLivroDto);
+  }
+}
