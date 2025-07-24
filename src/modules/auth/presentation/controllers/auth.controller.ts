35b5066a93bfa307790eafa1c3df3f5707a1f1bd
GabrielLourencoh <gl930551@gmail.com>
2025-07-24 12:54:17 -0300

    Adicionando documentação do swagger na rota de login

--- src/modules/auth/presentation/controllers/auth.controller.ts
+++ src/modules/auth/presentation/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { LoginAutorUseCase } from '../../application/use-cases/login-autor.use-case';
 import { LoginDto } from '../dtos/inputs/login.dto';
 import { LoginResponseDto } from '../dtos/outputs/login-response.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,10 @@ export class AuthController {
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Realizar login do autor' })
+  @ApiResponse({ status: 500, description: 'Erro interno do servidor' })
+  @ApiResponse({ status: 200, description: 'Login concluido' })
+  @ApiResponse({ status: 401, description: 'Sem autorização' })
   async login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
     const AuthTokenOutput = await this.loginAutorUseCase.execute(loginDto);
 
