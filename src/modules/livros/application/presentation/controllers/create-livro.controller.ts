9f1182789b7344dd0f6075ffd0e20a7f6c684b24
GabrielLourencoh <gl930551@gmail.com>
2025-07-26 18:40:13 -0300

    Adicionando um ApiOkResponse pro controller create dos livros

--- src/modules/livros/application/presentation/controllers/create-livro.controller.ts
+++ src/modules/livros/application/presentation/controllers/create-livro.controller.ts
@@ -1,7 +1,13 @@
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateLivroUseCase } from '../../use-cases/create-livro.use-case';
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CreateLivroDto } from '../dtos/inputs/create-livro.dto';
+import { CreateLivroOutputDto } from '../dtos/outputs/create-livro.output';
 
 @ApiTags('Livros')
 @Controller('livros')
@@ -14,6 +20,10 @@ export class CreateLivroController {
   @ApiResponse({ status: 201, description: 'Livro criado com sucesso' })
   @ApiResponse({ status: 409, description: 'ISBN duplicado' })
   @ApiResponse({ status: 500, description: 'Erro interno do servidor' })
+  @ApiOkResponse({
+    description: 'Livro criado com sucesso!',
+    type: CreateLivroOutputDto,
+  })
   async create(@Body() createLivroDto: CreateLivroDto) {
     // Delega a lógica de negócio para o Use Case.
     // O Use Case retorna a Entidade de Domínio Livro criada.
