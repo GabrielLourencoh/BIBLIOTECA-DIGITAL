ead5275a18bf489216dbba8d62ca3d008fbab489
GabrielLourencoh <gl930551@gmail.com>
2025-07-26 18:21:11 -0300

    Adicionando um ApiOkResponse pro controller de criação do autor

--- src/modules/autores/application/presentation/controllers/create-autor.controller.ts
+++ src/modules/autores/application/presentation/controllers/create-autor.controller.ts
@@ -1,7 +1,13 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CreateAutorUseCase } from 'src/modules/autores/application/use-cases/create-autor.use-case';
 import { CreateAutorDto } from '../dtos/inputs/create-autor.dto';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CreateAutorOutputDto } from '../dtos/outputs/create-autor.output';
 
 @ApiTags('Autores')
 @Controller('autores')
@@ -13,6 +19,10 @@ export class CreateAutorController {
   @ApiResponse({ status: 201, description: 'Autor criado com sucesso' })
   @ApiResponse({ status: 409, description: 'CPF duplicado' })
   @ApiResponse({ status: 500, description: 'Erro interno do servidor' })
+  @ApiOkResponse({
+    description: 'Autor criado com sucesso!',
+    type: CreateAutorOutputDto,
+  })
   @Post('/criar')
   async handle(@Body() createAutorDto: CreateAutorDto) {
     const novoAutor = await this.createAutorUseCase.execute(createAutorDto);
