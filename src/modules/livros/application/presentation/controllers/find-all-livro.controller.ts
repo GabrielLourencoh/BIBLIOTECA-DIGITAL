3861ff6c06af57b53dbb3260b9423dc03c1051bb
GabrielLourencoh <gl930551@gmail.com>
2025-07-26 18:41:53 -0300

    Adicionando um ApiOkResponse pro controller findAll dos livros

--- src/modules/livros/application/presentation/controllers/find-all-livro.controller.ts
+++ src/modules/livros/application/presentation/controllers/find-all-livro.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { FindAllLivroUseCase } from '../../use-cases/find-all-livro.use-case';
+import { FindAllLivrosOutputdto } from '../dtos/outputs/find-all-livros.output';
 
 @ApiTags('Livros')
 @Controller('livros')
@@ -8,11 +9,11 @@ export class FindAllLivroController {
   constructor(private readonly findAllLivroUseCase: FindAllLivroUseCase) {}
 
   @ApiOperation({ summary: 'Listar todos os livros' })
-  @ApiResponse({
-    status: 200,
-    description: 'Lista de livros retornada com sucesso',
-  })
   @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Livro criado com sucesso!',
+    type: FindAllLivrosOutputdto,
+  })
   @Get('/todos')
   async handle() {
     const livros = await this.findAllLivroUseCase.execute();
