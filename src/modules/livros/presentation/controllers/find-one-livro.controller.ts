a35f5d5f9438de55a6b6ccc8eb86204b18b65de6
GabrielLourencoh <gl930551@gmail.com>
2025-08-24 10:44:34 -0300

    Documentando a rota findOne para os Livros

--- src/modules/livros/presentation/controllers/find-one-livro.controller.ts
+++ src/modules/livros/presentation/controllers/find-one-livro.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { FindOneLivroUseCase } from '../../application/use-cases/find-one-livro.use-case';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { FindOneLivroOutputDto } from '../dtos/outputs/find-one-livro.output';
 
 @ApiTags('Livros')
 @Controller('livros')
 export class FindOneLivroController {
   constructor(private readonly findOneLivroUseCase: FindOneLivroUseCase) {}
 
+  @ApiOperation({ summary: 'Buscar um livro por ID' })
+  @ApiResponse({ status: 404, description: 'Livro não encontrado' })
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Livro encontrado com sucesso!',
+    type: FindOneLivroOutputDto,
+  })
   @Get(':id')
   async handel(@Param('id', ParseIntPipe) id: number) {
     const livro = await this.findOneLivroUseCase.execute(id);
