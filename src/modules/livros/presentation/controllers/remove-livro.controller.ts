40b4a2de438b20e6b8154db7d4f9521cd08512ef
GabrielLourencoh <gl930551@gmail.com>
2025-08-24 12:11:03 -0300

    docs: Documentando a rota de 'remove' para os Livros

--- src/modules/livros/presentation/controllers/remove-livro.controller.ts
+++ src/modules/livros/presentation/controllers/remove-livro.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Delete, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { RemoveLivroUseCase } from '../../application/use-cases/remove-livro.use-case';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { RemoveLivroOutputDto } from '../dtos/outputs/remove-livro.output';
 
 @ApiTags('Livros')
 @Controller('livros')
 export class RemoveLivroController {
   constructor(private readonly removeLivroUseCase: RemoveLivroUseCase) {}
 
+  @ApiOperation({ summary: 'Remover um livro por ID' })
+  @ApiResponse({ status: 404, description: 'Livro não encontrado' })
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Livro removido com sucesso!',
+    type: RemoveLivroOutputDto,
+  })
   @Delete(':id')
   async handle(@Param('id', ParseIntPipe) id: number) {
     const livroDeleted = await this.removeLivroUseCase.execute(id);
