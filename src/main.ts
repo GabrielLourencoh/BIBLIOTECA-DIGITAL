172174173377f7e53768406a6b40c9f7e13e87d9
GabrielLourencoh <gl930551@gmail.com>
2025-08-25 21:41:46 -0300

    style: adiciona 'void' na chamada de bootstrap para corrigir erro do eslint

--- src/main.ts
+++ src/main.ts
@@ -58,5 +58,4 @@ async function bootstrap() {
     console.log('------------------------------------');
   }
 }
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap();
+void bootstrap();
